bb4dfcca20cbb9d221a33ec49ec80ce7c1252496
Lolboom56 <timofemeyfsb@gmail.com>
2025-09-16 13:48:47 +0300

    Add files via upload

---
+++ hub3.pas
@@ -0,0 +1,38 @@
+﻿program Ye;
+
+var
+  x, y: integer; 
+
+begin
+  writeln('Введите год:');
+  readln(x);
+
+  
+
+  if x mod 4 = 0 then 
+  
+  begin
+    if x mod 100 = 0 then 
+    begin
+      if x mod 400 = 0 then 
+      begin
+         writeln('Високосный год'); 
+      end
+      else
+      begin
+        writeln('Не високосный год'); 
+      end;
+    end
+    else
+    begin
+       writeln('Високосный год'); 
+    end;
+
+  end
+  else
+  begin
+    writeln('Не високосный год'); 
+  end;
+
+   
+end.
